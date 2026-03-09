5d96fdfb68290a4972af3799ac5f3cdcc8509a8b
radik <radikmanasyan@gmail.com>
2026-03-09 17:55:26 +0400

    add bcrypt config to the app config

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# for bcrypt
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
